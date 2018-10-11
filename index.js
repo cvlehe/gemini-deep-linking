74c3510027d055fc01ebcffedf4ce9382ef7cbe5
Charlie von Lehe <vonlehec@gmail.com>
2018-10-11 12:14:01 -0400

    Checking if we only need aasa file

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
 var http = require('http');
-var json = {
-  "applinks": {
-    "apps": [],
-    "details": [
-      { 
-        "appID": "P4LZFPM2NH.com.gemini.ios",
-        "paths": [ "/mobile/*" ]
-      }
-    ]
-  }
-};
+
 var port = process.env.PORT || 3000;
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  res.end(JSON.stringify(json));
 }).listen(port);
